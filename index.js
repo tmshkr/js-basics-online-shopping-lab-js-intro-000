70523109e1c8803ae0df189e324a476fed9a8217
Tim Shaker <tmshkr@users.noreply.github.com>
2018-07-02 00:19:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function removeFromCart(item) {
     if (cart[i].itemName === item)
       cart.splice(i, 1)
   }
-  if (length !=== cart.length)
+  if (length !== cart.length)
     return 'That item is not in your cart.'
   else return cart
 }
