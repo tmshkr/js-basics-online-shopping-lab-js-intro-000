d13fecc93bf0059dee862257406c361fcb65bcf6
Tim Shaker <tmshkr@users.noreply.github.com>
2018-07-02 00:21:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,12 +38,14 @@ function total() {
 }
 
 function removeFromCart(item) {
-  var length = cart.length
+  var removed = 0
   for (let i = 0; i < cart.length; i++){
-    if (cart[i].itemName === item)
+    if (cart[i].itemName === item){
       cart.splice(i, 1)
+      removed++
+    }
   }
-  if (length !== cart.length)
+  if (removed === 0)
     return 'That item is not in your cart.'
   else return cart
 }
