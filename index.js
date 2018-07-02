963864acabe8e157a9e300f2ff6503050a50f086
Tim Shaker <tmshkr@users.noreply.github.com>
2018-07-02 00:23:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,5 +51,6 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === undefined)
+    return "Sorry, we don't have a credit card on file for you."
 }
